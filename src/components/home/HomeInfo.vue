e6fc8c8709219befe6061eb3dcf9358b11200482
SirCoffeeKnight <sircoffeeknight@gmail.com>
2021-04-22 14:53:02 +0200

    RSUN-7: Add Sidecards, Update Main Content Home

--- src/components/home/HomeInfo.vue
+++ src/components/home/HomeInfo.vue
@@ -1,5 +1,6 @@
 <template>
     <section>
         <h2>Welcome!</h2>
+        <p>I'm glad that you are here! Let me introduce myself!</p>
     </section>
 </template>
--- src/components/sidepanel/SidePanel.vue
+++ src/components/sidepanel/SidePanel.vue
@@ -1,10 +1,12 @@
 <template>
     <section>
         <base-side-card>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eum corrupti odit animi vero excepturi eius, et voluptatem ipsam culpa, atque quam sed doloribus cumque error dolore, quasi maiores optio!
+            <h3>Git-Repositories</h3>
+
         </base-side-card>
         <base-side-card>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eum corrupti odit animi vero excepturi eius, et voluptatem ipsam culpa, atque quam sed doloribus cumque error dolore, quasi maiores optio!
+            <h3>Quick Links</h3>
+            
         </base-side-card>
     </section>
 </template>
