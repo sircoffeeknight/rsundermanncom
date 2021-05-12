4ebd402c64aab3ec02d4fc7a771e0b46499d08f1
sircoffeeknight <sircoffeeknight@gmail.com>
2021-05-12 14:23:17 +0200

    RSUN-25: Fixed FinaFix project displaying incorrect links. - Updated the Description of FinaFix

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -21,10 +21,10 @@ export default {
                 },
                 {
                     title: 'FinaFix',
-                    description: 'The project I am currently working on. FinaFix is a small Symfony project. The Idea behind FinaFix is to allow the user to manage their finances including depts and a plan to get out of them.',
-                    skills: ['HTML', 'CSS', 'Symfony', 'MySQL'],
-                    link: 'https://finafix.raphael-sundermann.com/',
-                    repository: 'https://github.com/sircoffeeknight/finafix'
+                    description: 'The project I am currently working on. FinaFix is a small PHP project. The Idea behind FinaFix is, to allow the user to manage their finances, including depts, and a plan to get out of them. Currently I am deciding if I want to use Symfony or Laravel and if I want to include react or angular.',
+                    skills: ['HTML', 'CSS', 'PHP', 'MySQL'],
+                    link: '',
+                    repository: ''
                 },
             ]
         }
